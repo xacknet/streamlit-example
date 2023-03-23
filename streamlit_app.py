f07fb103a088add7ab6af511222c482f43a1c4fd
xacknet <46866194+xacknet@users.noreply.github.com>
2023-03-23 20:49:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,40 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Создаем заголовок
+st.title("Калькулятор стоимости рекультивации земель")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Создаем слайдер для выбора площади земли
+land_area = st.slider("Выберите площадь земли в квадратных метрах", 0, 100000, 1000)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Создаем выпадающее меню для выбора типа рекультивации
+reclamation_type = st.selectbox("Выберите тип рекультивации", ("Механическая", "Химическая", "Биологическая"))
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Создаем чекбоксы для выбора необходимых работ
+has_topsoil_removal = st.checkbox("Удаление верхнего слоя почвы")
+has_soil_stabilization = st.checkbox("Стабилизация грунта")
+has_landscaping = st.checkbox("Ландшафтный дизайн")
 
+# Вычисляем стоимость рекультивации на основе выбранных параметров
+cost_per_square_meter = 50
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if reclamation_type == "Химическая":
+    cost_per_square_meter += 20
+elif reclamation_type == "Биологическая":
+    cost_per_square_meter += 30
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+if has_topsoil_removal:
+    cost_per_square_meter += 10
+if has_soil_stabilization:
+    cost_per_square_meter += 15
+if has_landscaping:
+    cost_per_square_meter += 5
 
-    points_per_turn = total_points / num_turns
+total_cost = land_area * cost_per_square_meter
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Выводим результаты на экран
+st.write(f"Площадь земли: {land_area} кв. м.")
+st.write(f"Тип рекультивации: {reclamation_type}")
+st.write(f"Удаление верхнего слоя почвы: {has_topsoil_removal}")
+st.write(f"Стабилизация грунта: {has_soil_stabilization}")
+st.write(f"Ландшафтный дизайн: {has_landscaping}")
+st.write(f"Общая стоимость рекультивации: {total_cost} рублей")
